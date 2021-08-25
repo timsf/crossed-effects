45446015c4e8c599c10c3d2b27761dc6a2e51fda
timsf <timstf@gmail.com>
2021-08-25 11:41:02 +0100

    fix: pseudo-data representation for augmented binomial algorithm

--- xfx/custom/binomial.py
+++ xfx/custom/binomial.py
@@ -29,7 +29,7 @@ def sample_posterior(y: np.ndarray, n: np.ndarray, j: np.ndarray, i: np.ndarray,
         alp0, alp = alp[0][0], alp[1:]
 
     while True:
-        x0, x1, x2 = xfx.lm.gibbs.reduce_data((y - n / 2) / nu, np.zeros_like(y), nu, i)
+        x0, x1, x2 = xfx.lm.gibbs.reduce_data(y - n / 2, np.zeros_like(y), nu, i)
         alp = xfx.lm.gibbs.update_coefs(x1, x2, None, alp, tau, 1, ome)
         alp0 = xfx.lm.gibbs.update_intercept(x0, x1, alp, 1, ome)
         if not np.all(np.isinf(prior_n_tau)):
@@ -58,17 +58,19 @@ def sample_jacobi(z: float, ome: np.random.Generator, t: float = .64) -> float:
 
     th1 = 1 / np.abs(z)
     th2 = z**2/2 + np.pi**2/8
-    p1 = 2 * np.exp(-np.abs(z)) * invgauss(th1).cdf(t)
-    p2 = np.pi * np.exp(-th2 * t) / (2 * th2)
-    p = p1 / (p1 + p2)
+    log_p1 = np.log(2) - np.abs(z) + invgauss(th1).logcdf(t)
+    log_p2 = np.log(np.pi) - th2 * t - np.log(2 * th2)
+    log_p = log_p1 - np.logaddexp(log_p1, log_p2)
     while True:
-        if ome.uniform() < p:
+        if np.log(ome.uniform()) < log_p:
             x = sample_rtrunc_invgauss(th1, t, ome)
         else:
             x = sample_ltrunc_exp(th2, t, ome)
         series = series_jacobi(x, z, t)
         u = ome.uniform(0, next(series))
         for n, s in enumerate(series, 1):
+            if n > 1:
+                raise Exception
             if bool(n % 2) and u <= s:
                 return x
             if not bool(n % 2) and s < u:
@@ -84,7 +86,7 @@ def sample_rtrunc_invgauss(mu: float, t: float, ome: np.random.Generator) -> flo
                 if e1 ** 2 <= 2 * e2 / t:
                     break
             x = t / (1 + t * e1) ** 2
-            if ome.uniform() <= np.exp(-x / (2 * mu ** 2)):
+            if np.log(ome.uniform()) <= -x / (2 * mu ** 2):
                 return x
     else:
         while True:
@@ -92,7 +94,7 @@ def sample_rtrunc_invgauss(mu: float, t: float, ome: np.random.Generator) -> flo
             if x < t:
                 return x
 
-        
+
 def sample_ltrunc_exp(lam: float, t: float, ome: np.random.Generator) -> float:
 
     return t + ome.exponential() / lam
